2baa855d79acc8c150757be3f3f14ccffcda97f0
Pai Ge <gavin.gepai@gmail.com>
2019-05-26 21:32:10 -0700

    Create project

---
+++ app/src/main/java/com/paige/trysomethingnew/TrySomethingNewApplication.kt
@@ -0,0 +1,9 @@
+package com.paige.trysomethingnew
+
+import android.app.Application
+
+class TrySomethingNewApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/paige/trysomethingnew/api/service/YelpApiService.kt
@@ -0,0 +1,5 @@
+package com.paige.trysomethingnew.api.service
+
+public interface YelpApiService {
+
+}
